9821604ea9823989006d7221f544ceaeb88881ff
Сергей <66009294+khokh10v@users.noreply.github.com>
2020-09-19 19:49:19 +0300

    df

--- index.js
+++ index.js
@@ -7,40 +7,40 @@ var credentials = {key: privateKey, cert: certificate};
 
 const express = require('express')
 const app = express();
-// const server = https.createServer(credentials, app);  // запускаем сервак 
-// const io = require('socket.io')(server)               // сокеты к нашему серверу
-// const { v4: uuidV4 } = require('uuid')                // это приблуда для создания комнат
+const server = https.createServer(credentials, app);  // запускаем сервак 
+const io = require('socket.io')(server)               // сокеты к нашему серверу
+const { v4: uuidV4 } = require('uuid')                // это приблуда для создания комнат
 
-// //----------------------
-// // Пример видеочата
-// //----------------------
-// app.set('view engine', 'ejs')                         // шаблонизатор для node
-// app.use(express.static('public'))                     // путь к папке с шаблоними
+//----------------------
+// Пример видеочата
+//----------------------
+app.set('view engine', 'ejs')                         // шаблонизатор для node
+app.use(express.static('public'))                     // путь к папке с шаблоними
 
-// app.get('/', (req, res) => {
-//   res.redirect(`/${uuidV4()}`)                        // перенаправляем на комнату ( уникальный id url )
-// })
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)                        // перенаправляем на комнату ( уникальный id url )
+})
 
-// app.get('/:room', (req, res) => {
-//   res.render('room', { roomId: req.params.room })     // создаем комнату и получаем id
-// })
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })     // создаем комнату и получаем id
+})
 
-// io.on('connection', socket => {
-//   // Образуем комнаты для socket соединений
-//   socket.on('join-room', (room_id, user_id) => {
-//     console.log('Приконектился пользователь: ' + room_id + ', \n' + user_id)
-//     socket.join(room_id)
-//     socket.to(room_id).broadcast.emit('user-connected', user_id)
+io.on('connection', socket => {
+  // Образуем комнаты для socket соединений
+  socket.on('join-room', (room_id, user_id) => {
+    console.log('Приконектился пользователь: ' + room_id + ', \n' + user_id)
+    socket.join(room_id)
+    socket.to(room_id).broadcast.emit('user-connected', user_id)
 
-//     socket.on('chat', (msg) => {
-//       io.emit('chat', msg, socket.id);
-//       console.log('message: ' + socket.id + ' => ' + msg);
-//     });
-//     // socket.on('disconnect', () => {
-//     //   socket.to(roomId).broadcast.emit('user-disconnected', userId)
-//     // })
-//   })
-// })
+    socket.on('chat', (msg) => {
+      io.emit('chat', msg, socket.id);
+      console.log('message: ' + socket.id + ' => ' + msg);
+    });
+    // socket.on('disconnect', () => {
+    //   socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    // })
+  })
+})
 
 
 
@@ -51,7 +51,7 @@ const app = express();
 
 let port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.send('hellow world 2')                        // перенаправляем на комнату ( уникальный id url )
+  res.send('hellow world 3')                        // перенаправляем на комнату ( уникальный id url )
 })
 
 app.listen(port, () => {
